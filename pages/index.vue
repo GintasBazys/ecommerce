d695718b37f4ea3f11269123aad8ca3a653dd110
GintasBazys <bazys.gintas@gmail.com>
2025-05-17 09:17:00 +0300

    hydration fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import MainBanner from "@/components/Banner/MainBanner.vue"
+import AboutCta from "@/components/About/AboutCta.vue"
+import BestSellers from "@/components/Products/BestSellers.vue"
+import LatestProducts from "@/components/Products/LatestProducts.vue"
+import LatestPosts from "@/components/About/LatestPosts.vue"
+
 useHead({
     title: "Shop | Ecommerce",
     script: [
